8ce3127ce841ea28f87d0b983f136a45fa304475
Nadia <belozerovany@gmail.com>
2017-07-04 22:10:17 +0300

    first page

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,15 @@
-$(function() {
+/*$(function(){
+	 
 
-	// Пользовательские функции
-
-});
+});*/
+	console.log("fff");
+	var mySwiper = new Swiper(document.querySelector('.swiper-container'), {
+		pagination: '.swiper-pagination',
+		paginationClickable: true,
+	    speed: 300,
+	    spaceBetween: 100,
+	    autoplay:5000,
+	    auto:4000,
+	    nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev'
+	}); 
