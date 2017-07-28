ac5af581ac2768c2132cbe27e5d83f13a8a69dce
Nadia <belozerovany@gmail.com>
2017-07-28 22:00:22 +0300

    pull

--- app/js/common.js
+++ app/js/common.js
@@ -1,14 +1,16 @@
-/*$(function(){
-	 
 
-});*/
-	console.log("fff");
-	var mySwiper = new Swiper(document.querySelector('.swiper-container'), {
+$(document).ready(function (){
+	var mySwiper = new Swiper('.swiper-container', {
 		pagination: '.swiper-pagination',
 		paginationClickable: true,
-	    speed: 300,
-	    autoplay:5000,
-	    auto:4000,
-	    nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev'
-	}); 
+		speed: 300,
+		autoplay:5000,
+		auto:4000,
+		nextButton: '.swiper-button-next',
+		prevButton: '.swiper-button-prev'
+	});
+
+    $(".hamburger").click(function(){
+        $(this).toggleClass("is-active");
+    });
+});
